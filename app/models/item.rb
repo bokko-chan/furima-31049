2cb43601056200256e08b7869dd218a560728c3a
bokko-chan <72552210+bokko-chan@users.noreply.github.com>
2020-11-08 02:36:48 +0900

    商品出品ページ表示, 出品データ保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,13 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associatons::ActiveRecordExtensions
-  belongs_to :user, :category, :status, :carriage, :area, :send
-  has_one_attached :image
-  with_options presence: ture do
-    validates :title, :explain
-    validates :price, format: { /\A[0-9]+\z/ }
+  with_options presence: true do
+    validates :image, :title, :explain
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
   validates :category_id, :status_id, :carriage_id, :area_id, :send_id, numericality: { other_than: 1 }
+
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :carriage, :area, :send
+
 end
