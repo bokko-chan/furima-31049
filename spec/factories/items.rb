c5d03cc99f4f14b11d76f25c1c7405fc516f8183
bokko-chan <72552210+bokko-chan@users.noreply.github.com>
2020-11-08 19:10:47 +0900

    Itemモデル単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    title {Faker::Lorem.sentence}
+    explain {Faker::Lorem.sentence}
+    category_id {2}
+    status_id {2}
+    carriage_id {2}
+    area_id {2}
+    send_id {2}
+    price {Faker::Number.within(range:300..9999999)}
+    association :user
+
+    after(:build) do |img|
+      img.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image_jpg')
+    end
   end
 end
