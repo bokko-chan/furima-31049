0542ab9aa6a43b7217941a55a603ed9e7ef0ef90
bokko-chan <72552210+bokko-chan@users.noreply.github.com>
2020-11-16 14:55:41 +0900

    user本名バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,9 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname, :birthday
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-    validates :family_name, :first_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-    validates :family_name_kana, :first_name_kana, format: { with: /\p{katakana}/ }
+    # validates :family_name, :first_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+    # validates :family_name_kana, :first_name_kana, format: { with: /\p{katakana}/ }
+    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
   end
 end 
